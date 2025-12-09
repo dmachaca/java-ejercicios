722acba9c4ab4561d0d264bcb0518be0e7260449
dmachaca <dmachacass@gmail.com>
2025-12-09 01:00:51 -0500

    feat: se cambia descripcion

--- src/main/java/pe/com/den/Main.java
+++ src/main/java/pe/com/den/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese su nombre: ");
+        System.out.print("Ingrese un nombre: ");
         String nombre = scanner.next();
         System.out.println("¡Hola "+nombre+"!");
     }
